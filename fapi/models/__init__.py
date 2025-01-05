5acb1cd41796a0df459ac4efe1607396f44d4463
Palash <ariful.islamk777@gmail.com>
2025-01-05 20:13:02 +0600

    settings updated

---
+++ fapi/models/__init__.py
@@ -0,0 +1,5 @@
+from fapi.models.base_model import BaseModel
+from fapi.models.product import Product
+
+__all__ = ['BaseModel']
+__all__ += ['Product']
---
+++ fapi/models/base_model.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    data_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    json_meta = models.JSONField(blank=True, null=True)
+    type = models.CharField(max_length=255, blank=True, null=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
