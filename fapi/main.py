e6f3d8e8f74fb7e21a643745d7e88f3323f9ea69
Palash <ariful.islamk777@gmail.com>
2025-01-05 19:32:29 +0600

    added create_products, delete_products API

--- fapi/main.py
+++ fapi/main.py
@@ -1,12 +1,15 @@
 import os
 import django
 from asgiref.sync import sync_to_async
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-from fastapi import FastAPI
-from products.models import Product
+from fastapi import FastAPI, HTTPException
+from fapi.models.product import Product, CreateProduct
+from typing import List
+from django.db import transaction
 app = FastAPI()
+
+
 
 @app.get("/products")
 async def get_products():
@@ -21,3 +24,37 @@ async def get_products():
         }
         for product in products
     ]
+
+@app.post("/products")
+async def create_products(products_data: List[dict]):
+    created_products = []
+    for data in products_data:
+        product = Product(
+            name=data["name"],
+            description=data["description"],
+            price=data["price"],
+            stock=data["stock"],
+        )
+        await sync_to_async(product.save)()
+        created_products.append(product)
+
+    response = [
+        {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": str(product.price),
+            "stock": product.stock,
+        }
+        for product in created_products
+    ]
+
+    return response
+
+@app.delete("/products")
+async def delete_products(product_ids: list[int]):
+    deleted_count, _ = await sync_to_async(Product.objects.filter(id__in=product_ids).delete)()
+    if deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Product Not Found")
+    return {"message": "Products have been deleted successfully."}
+
